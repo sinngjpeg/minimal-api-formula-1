8e110c09f9a553eee4c2d115c562f01bf1fc45a3
Ingrid Silva <ingridsilvatec@outlook.com>
2025-07-15 11:20:29 -0300

    add drivers and teams

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,29 @@
-console.log("hello world");
+import fastify from "fastify";
+
+const server = fastify({logger: true});
+
+const teams = [
+    {id: 1, name:"MacLaren", base: "Woking, United Kingdom"},
+    {id: 2, name:"Red Bull", base: "Milton Keynes, United Kingdom"},
+    {id: 3, name:"Mercedes", base: "Brackley, United Kingdom"}
+]; 
+
+const drivers = [
+    {id: 1, name:"Max Verstappen", team: "Red Bull"},
+    {id: 2, name: "Luis Hamilton", team: "Ferrari"},
+    {id:3, name: "Lando Norris", teams: "McLaren"}
+];
+
+server.get("/teams", async(request, response) =>{
+    response.type("application/json").code(200);
+    return {teams};
+});
+
+server.get("/drivers", async(request, response) =>{
+    response.type("application/json").code(200);
+    return [{id:1, name:"Max Verstappen", team: "Red Bull"}];
+})
+
+server.listen({port: 3333}, () => {
+    console.log("Server init");
+});
